f730ca31463cc4ca619f390bd3fd3ad11277a500
Unknown <macintosh.codeuse@gmail.com>
2018-09-09 21:26:40 +0200

    modification dynamique du nom de l'appareil (two-way binding)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ApplianceItemComponent } from './appliance-item/appliance-item.component';
@@ -10,7 +11,8 @@ import { ApplianceItemComponent } from './appliance-item/appliance-item.componen
 		ApplianceItemComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		FormsModule
 	],
 	providers: [],
 	bootstrap: [
