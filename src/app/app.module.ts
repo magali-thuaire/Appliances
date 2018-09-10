31d578bf7f71f43cb6a118b6eae345cad3e96a06
Unknown <macintosh.codeuse@gmail.com>
2018-09-10 09:14:37 +0200

    ajout du service appliance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ApplianceItemComponent } from './appliance-item/appliance-item.component';
+import { ApplianceService } from './services/appliance.service';
 
 @NgModule({
 	declarations: [
@@ -14,7 +15,9 @@ import { ApplianceItemComponent } from './appliance-item/appliance-item.componen
 		BrowserModule,
 		FormsModule
 	],
-	providers: [],
+	providers: [
+		ApplianceService
+	],
 	bootstrap: [
 		AppComponent
 	]
