8e199d88ac09116c6ef12da1f5113b1eb388719c
Unknown <magali_thuaire@yahoo.fr>
2018-09-13 11:26:11 +0200

    utilisation d'une observable pour la durée de connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit, OnDestroy {
 
-	constructor() {}
+	secondes: number;
+	counterSubscription: Subscription;
+
+	constructor() { }
+
+	// Initialisation
+	ngOnInit() {
+
+		// Création d'une observable
+		const counter = interval(1000);
+
+		// Observation de l'observable
+		this.counterSubscription = counter.subscribe(
+			(value: number) => {
+				this.secondes = value;
+			}
+		);
+	}
+
+	// Destruction
+	ngOnDestroy() {
+		this.counterSubscription.unsubscribe();
+	}
 
 }
